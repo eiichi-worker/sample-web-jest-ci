a83b2da0479ccc93d5f181d6000764d0da7e3ee4
Eiichi Fujimoto <work.eiichi.co.jp@gmail.com>
2018-02-15 02:23:12 +0900

    refactor: テストにコメントを追加

--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -1,13 +1,14 @@
+// テスト対象クラスの読み込み
 var Calculator = require('../my_modules/Calculator')
-var calculator = new Calculator()
+var calc = new Calculator()
 
-describe('Calculator', () => {
-  describe('sum()', () => {
-    it('sum(1, 2) = 3', () => {
-      expect(calculator.sum(1, 2)).toBe(3)
+describe('Calculator', () => {  // Calculatorクラスの
+  describe('sum()', () => {     // sum()メソッドで
+    it('sum(1, 2) = 3', () => { // 1, 2を渡したら3が返ってくるかテスト
+      expect(calc.sum(1, 2)).toBe(3)
     })
-    it('sum("99", "100") = 199', () => {
-      expect(calculator.sum('99', '100')).toBe(199)
+    it('sum("99", "100") = 199', () => { // "99", "100"を渡したら199が返ってくるかテスト
+      expect(calc.sum('99', '100')).toBe(199)
     })
   })
 })
